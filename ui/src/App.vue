2ee62ff95f3b6f6edec5d415b265672c6e6b25e8
Igor Milosavljevic <91330526+igor570@users.noreply.github.com>
2025-07-04 20:46:19 +0100

    init

---
+++ ui/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div>Hello</div>
+  <Button variant="outline">Button from Shadcn</Button>
+</template>
+
+<style scoped></style>
