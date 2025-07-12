c557faa35af9f86a4f80855cd20e8bbcb143015a
Igor Milosavljevic <91330526+igor570@users.noreply.github.com>
2025-07-12 12:22:57 +0100

    connected link to download

--- ui/src/composables/index.ts
+++ ui/src/composables/index.ts
@@ -1,7 +1,4 @@
 export * from './useAuth'
 export * from './useCreateMessage'
-<<<<<<< Updated upstream
 export * from './useMessageHistory'
-=======
 export * from './useGetMessage'
->>>>>>> Stashed changes
