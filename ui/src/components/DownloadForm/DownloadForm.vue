2c31d3dfd9049dc484d9e1f8ba69d946c9d5f271
Igor Milosavljevic <migor@live.co.uk>
2025-07-20 13:04:15 +0100

    removed import

--- ui/src/components/DownloadForm/DownloadForm.vue
+++ ui/src/components/DownloadForm/DownloadForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, defineEmits } from 'vue'
+import { computed, ref } from 'vue'
 import { passwordFormSchema } from '@/types'
 import { useForm, Field } from 'vee-validate'
 
